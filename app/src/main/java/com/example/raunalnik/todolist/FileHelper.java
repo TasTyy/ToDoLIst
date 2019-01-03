3b3a38f224fcfcd97ce1e132df42ac78ca58f9e5
TasTyy <43649526+TasTyy@users.noreply.github.com>
2019-01-03 19:09:29 +0100

    Popravki

--- app/src/main/java/com/example/raunalnik/todolist/FileHelper.java
+++ app/src/main/java/com/example/raunalnik/todolist/FileHelper.java
@@ -2,7 +2,12 @@ package com.example.raunalnik.todolist;
 
 import android.content.Context;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class FileHelper {
@@ -11,8 +16,35 @@ public class FileHelper {
 
     public static void writeData(ArrayList<String> opravila, Context context) {
 
-        FileOutputStream fos = context.openFileOutput(FILENAME)
+        try {
+            FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(opravila);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+
+    public static ArrayList<String> readData(Context context) {
+        ArrayList<String> listOpravil = null;
+        try {
+            FileInputStream fis = context.openFileInput(FILENAME);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            listOpravil = (ArrayList<String>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            listOpravil = new ArrayList<>();
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return listOpravil;
+    }
 
 }
--- app/src/main/java/com/example/raunalnik/todolist/MainActivity.java
+++ app/src/main/java/com/example/raunalnik/todolist/MainActivity.java
@@ -3,16 +3,18 @@ package com.example.raunalnik.todolist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     private EditText dodaj_opravilo;
     private Button dodaj_button;
@@ -31,7 +33,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dodaj_button = findViewById(R.id.dodaj_btn);
         display_opravila = findViewById(R.id.display_opravila);
 
+        opravila = FileHelper.readData(this);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, opravila);
+        display_opravila.setAdapter(adapter);
+
         dodaj_button.setOnClickListener(this);
+        display_opravila.setOnItemClickListener(this);
     }
 
     @Override
@@ -42,9 +50,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 adapter.add(itemEntered);
                 dodaj_opravilo.setText("");
 
+                FileHelper.writeData(opravila, this);
+
                 Toast.makeText(this, "Opravilo dodano", Toast.LENGTH_SHORT).show();
 
                 break;
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        opravila.remove(position);
+        adapter.notifyDataSetChanged();
+        FileHelper.writeData(opravila, this);
+
+        Toast.makeText(this, "Izbrisano", Toast.LENGTH_SHORT).show();
     }
 }
